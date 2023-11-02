c2a3f720200898f06bc559aa5e606627d8b3eadb
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2023-11-02 18:23:32 +0300

    install.py
    هذا الملف هو الخاص بتنزيل المكتبات باللغة بايثون 
    
    This file is for downloading libraries in Python

---
+++ install.py
@@ -0,0 +1,22 @@
+import os
+import pyfiglet
+import random
+from termcolor import colored
+
+colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+
+selected_color = random.choice(colors)
+
+text = 'I N S T A L L'
+
+lo = pyfiglet.figlet_format(text)
+colored_lo = colored(lo, color=selected_color)
+
+print(colored_lo)
+
+os.system("pip install speedtest-cli")
+os.system("pip install pyfiglet")
+os.system("pip install psutil")
+os.system("pip install termcolor")
+os.system("pip install colorama")
+os.system("pip install tqdm")
