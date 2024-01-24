acf3d34cb3d2717409e6f6ddd88829a0a3084694
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-01-24 09:06:31 +0300

    update.py

--- update.py
+++ update.py
@@ -25,7 +25,7 @@ def wait_with_spinner(seconds):
 
     for _ in range(int(seconds)):
         for symbol in symbols:
-            sys.stdout.write(f"\rPlease wait {symbol}  ")
+            sys.stdout.write(f"\r[+] Please wait... {symbol}  ")
             sys.stdout.flush()
             time.sleep(0.25)
 
@@ -34,24 +34,5 @@ def wait_with_spinner(seconds):
 wait_time = 2.5
 wait_with_spinner(wait_time)
 
-def print_loading():
-    loading_text = colored("[*]", "blue") + colored("Loading", "red") + colored("...", "yellow")
-    
-    for char in loading_text:
-        print(char, end='', flush=True)
-        time.sleep(0.1)
-
-print_loading()
-
-print("")
 
 os.system("git pull origin master")
-
-
-text_part_1 = colored("[*]", "red")
-
-text_part_2 = colored("COMPLETE", "green")
-
-colored_text = f"{text_part_1} {text_part_2}"
-
-print(colored_text)
