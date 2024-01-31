3e01c459b0c9b4296c5d7ad1ce759163390df524
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-02-01 00:39:04 +0300

    update.py

--- update.py
+++ update.py
@@ -1,38 +1,13 @@
 import os 
-import sys
-import time 
-from time import sleep
-from colorama import Fore, Style
-from termcolor import colored
-import pyfiglet
-import random
-
 os.system("clear")
 
-colors = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
-
-selected_color = random.choice(colors)
-
-text = 'UPDATE'
-
-lo = pyfiglet.figlet_format(text)
-colored_lo = colored(lo, color=selected_color)
-
-print(colored_lo)
-
-def wait_with_spinner(seconds):
-    symbols = "/-|\\"
-
-    for _ in range(int(seconds)):
-        for symbol in symbols:
-            sys.stdout.write(f"\r[+] Please wait... {symbol}  ")
-            sys.stdout.flush()
-            time.sleep(0.25)
-
-    sys.stdout.write("\r" + " " * 20 + "\r")
-
-wait_time = 2.5
-wait_with_spinner(wait_time)
-
+print('''
+██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗
+██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
+██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  
+██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  
+╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗
+ ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝
+''')
 
 os.system("git pull origin master")
