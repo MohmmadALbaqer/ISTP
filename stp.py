69c450ca0bd80a03cd682778520233a1e0f31450
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-03-06 19:23:00 +0300

    Update stp.py

--- stp.py
+++ stp.py
@@ -5,8 +5,9 @@ import speedtest
 import requests
 from time import sleep
 from tqdm import tqdm
-from colorama import Fore, Style, init
+from colorama import Fore, Style, Back, init
 from termcolor import colored
+from prettytable import PrettyTable
 
 def clear_screen():
     operating_system = os.name
@@ -20,6 +21,7 @@ def clear_screen():
             print("Unsupported operating system.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
 clear_screen()
 
 def spin():
@@ -53,24 +55,10 @@ def check_internet_connection():
 if check_internet_connection():
     print(f"{Fore.GREEN}[*] Internet connection is available. You can proceed with execution.{Style.RESET_ALL}")
     time.sleep(0.25)
-
+    os.system("clear")
 else:
     print(f"{Fore.YELLOW}[{Fore.RED}!{Fore.YELLOW}]{Fore.RED} No internet connection !{Style.RESET_ALL}")
     exit()
-    
-def clear_screen():
-    operating_system = os.name
-
-    try:
-        if operating_system == 'posix': 
-            os.system('clear')
-        elif operating_system == 'nt': 
-            os.system('cls')
-        else:
-            print("Unsupported operating system.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-clear_screen()
 
 print(f"""{Fore.BLUE}
   ____________________________ 
@@ -78,7 +66,7 @@ print(f"""{Fore.BLUE}
  \_____  \  |    |   |     ___/
  /        \ |    |   |    |    
 /_______  / |____|   |____|  {Fore.GREEN}Version : 3{Fore.BLUE}
-        \/ {Style.RESET_ALL}""")
+        \/{Style.RESET_ALL}""")
 print(f'''
 {Fore.RED}+------------------------------------------------------------------+
 {Fore.RED}|{Fore.GREEN} GitHub{Fore.WHITE} : {Fore.BLUE}MohmmadALbaqer {Fore.WHITE}|{Fore.YELLOW} https://www.github.com/MohmmadALbaqer/ {Fore.RED}|
@@ -86,8 +74,8 @@ print(f'''
 {Fore.RED}+------------------------------------------------------------------+{Style.RESET_ALL}''')
 
 init(autoreset=True)
-print(f"{Fore.GREEN}[*] GETTING BEST AVAILABLE SERVERS, UPLOADING & DOWNLOADING SPEED.....{Style.RESET_ALL}")
-
+print(f"{Fore.WHITE}[{Fore.GREEN}*{Fore.WHITE}] {Fore.BLUE}Downloading to servers and information at internet speed{Style.RESET_ALL}")
+print(1*"\n")
 st = speedtest.Speedtest()
 st.get_best_server()
 
@@ -107,11 +95,18 @@ res_dict = st.results.dict()
 dwnl = f"{res_dict['download'] / 10**6:.2f}"
 upl = f"{res_dict['upload'] / 10**6:.2f}"
 
-print("")
-print(f"{Fore.WHITE}[{Fore.GREEN}*{Fore.WHITE}] {Fore.BLUE}Internet test information{Style.RESET_ALL}")
-print(f"{Fore.WHITE}[{Fore.GREEN}*{Fore.WHITE}] {Fore.YELLOW}Download:{Fore.BLUE} {dwnl} Mbps ({float(dwnl) * 0.125:.2f} MB/s) ")
-print(f"{Fore.WHITE}[{Fore.GREEN}*{Fore.WHITE}] {Fore.YELLOW}Upload:{Fore.BLUE} {upl} Mbps ({float(dwnl) * 0.125:.2f} MB/s)")  
-print(f"{Fore.WHITE}[{Fore.GREEN}*{Fore.WHITE}] {Fore.YELLOW}Ping:{Fore.BLUE} {res_dict['ping']:.2f} ms")
-print(f"{Fore.WHITE}[{Fore.GREEN}*{Fore.WHITE}] {Fore.YELLOW}HOST:{Fore.BLUE} {res_dict['server']['host']}") 
-print(f"{Fore.WHITE}[{Fore.GREEN}*{Fore.WHITE}] {Fore.YELLOW}SPONSOR:{Fore.BLUE} {res_dict['server']['sponsor']}")  
-print(f"{Fore.WHITE}[{Fore.GREEN}*{Fore.WHITE}] {Fore.YELLOW}LATENCY:{Fore.BLUE} {res_dict['server']['latency']:.2f} ms")
+table = PrettyTable()
+
+table.field_names = [f"{Fore.MAGENTA}ID{Style.RESET_ALL}", f"{Fore.BLUE}INFORMATION{Style.RESET_ALL}", f"{Fore.RED}Information results{Style.RESET_ALL}"]
+
+table.add_row([f"{Fore.GREEN}1{Style.RESET_ALL}", f"{Fore.YELLOW}Download{Style.RESET_ALL}", f"{dwnl} Mbps ({float(dwnl) * 0.125:.2f} MB/s)"])
+table.add_row([f"{Fore.GREEN}2{Style.RESET_ALL}", f"{Fore.YELLOW}Upload{Style.RESET_ALL}", f"{upl} Mbps ({float(upl) * 0.125:.2f} MB/s)"])
+table.add_row([f"{Fore.GREEN}3{Style.RESET_ALL}", f"{Fore.YELLOW}Ping{Style.RESET_ALL}", f"{res_dict['ping']:.2f} ms"])
+table.add_row([f"{Fore.GREEN}4{Style.RESET_ALL}", f"{Fore.YELLOW}HOST{Style.RESET_ALL}", res_dict['server']['host']])
+table.add_row([f"{Fore.GREEN}5{Style.RESET_ALL}", f"{Fore.YELLOW}SPONSOR{Style.RESET_ALL}", res_dict['server']['sponsor']])
+table.add_row([f"{Fore.GREEN}6{Style.RESET_ALL}", f"{Fore.YELLOW}LATENCY{Style.RESET_ALL}", f"{res_dict['server']['latency']:.2f} ms"])
+
+for field in table.field_names:
+    table.align[field] = "l"
+
+print(table)
