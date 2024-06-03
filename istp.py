ca6cfe141dac16233963ba73d04a80db8befbd8b
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-06-04 01:07:21 +0300

    Update istp.py

--- istp.py
+++ istp.py
@@ -4,13 +4,15 @@ import time
 import datetime
 import speedtest
 import requests
+import subprocess
+import platform
+import re
 from time import sleep
 from tqdm import tqdm
-from colorama import Fore, Style, Back, init
+from colorama import Back, init
 from termcolor import colored
 from prettytable import PrettyTable
-
-init()
+import threading
 
 R = "\033[91;1m"  # Red
 G = "\033[92;1m"  # Green
@@ -20,19 +22,37 @@ C = "\033[96;1m"  # Cyan
 M = "\033[95;1m"  # Magenta
 W = "\033[97;1m"  # White
 D = "\033[90;1m"  # Grey
-INFO = f"{B}[{G}INFO{B}]{M}"
+S = "\033[0m"
+
+sign = "\033[92;1m" + "[" + "\033[94;1m" + "*" + "\033[92;1m" + "]" + "\033[94;1m"
+Enter = "\033[94;1m" + "[" + "\033[92;1m" + "+" + "\033[94;1m" + "]" + "\033[92;1m"
+ERROR = "\033[93;1m" + "[" + "\033[91;1m" + "ERROR" + "\033[93;1m" + "]" + "\033[91;1m"
+INFO = "\033[93;1m" + "[" + "\033[92;1m" + "INFO" + "\033[93;1m" + "]" + "\033[94;1m"
+warning = "\033[93;1m" + "[" + "\033[91;1m" + "WARNING" + "\033[93;1m" + "]" + "\033[91;1m"
+Complete = "\033[94;1m" + "[" + "\033[92;1m" + "COMPLETE" + "\033[94;1m" + "]" + "\033[92;1m"
+Failed = "\033[93;1m" + "[" + "\033[91;1m" + "FAILED" + "\033[93;1m" + "]" + "\033[91;1m"
+please = "\033[93;1m" + "[" + "\033[91;1m" + "!" + "\033[93;1m" + "]" + "\033[91;1m"
+Question = "\033[95;1m" + "[" + "\033[96;1m" + "?" + "\033[95;1m" + "]" + "\033[97;1m"
+Help = "\033[97;1m" + "To continue anyway press or click" + "\033[94;1m" + " [" + "\033[92;1m" + "Enter" + "\033[94;1m" + "] " + "\033[97;1m" + "and to stop or exit" + "\033[93;1m" + " [" + "Ctrl" + "\033[97;1m" + " + " + "\033[93;1m" + "C" + "]" + "\033[0m"
+
+now = datetime.datetime.now()
+formatted_time = now.strftime("%I:%M %p")
+formatted_day = now.strftime("%A")
+
+date_day = "\033[94;1m" + "[" + "\033[92;1m" + "Today" + "\033[94;1m" + "]" + "\033[97;1m" + "(" + "\033[93;1m" + formatted_day + "\033[95;1m" + f" {now:%B %D %Y}" + "\033[97;1m" + ")" + "\033[94;1m" + "[" + "\033[92;1m" + "Time" + "\033[94;1m" + "]" + "\033[93;1m" + "[" + "\033[91;1m" + formatted_time + "\033[93;1m" + "]" + "\033[97;1m"
 
 def clear_screen():
     operating_system = os.name
     try:
-        if operating_system == 'posix': 
+        if (operating_system == 'posix'):
             os.system('clear')
-        elif operating_system == 'nt': 
+        elif (operating_system == 'nt'):
             os.system('cls')
         else:
-            print(f"[!] System unknown!{Style.RESET_ALL}")
+            print(f"{please} System unknown!{S}")
     except Exception as e:
-        print(f"[ERROR]{W}: {e}")
+        print(f"{ERROR}{W}: {e}")
+
 clear_screen()
 
 def spin():
@@ -50,7 +70,7 @@ def spin():
     sys.stdout.write("\r")
     sys.stdout.flush()
     done_message = colored("[+] Your Internet connection has been verified", 'yellow', attrs=['bold'])
-    sys.stdout.write("\033[K") 
+    sys.stdout.write("\033[K")
     print(done_message)
     time.sleep(1)
 
@@ -64,86 +84,175 @@ def check_internet_connection():
         return False
 
 if check_internet_connection():
-    print(f"{G}[*] Internet connection is available. You can proceed with execution.{W}")
+    print(f"{sign} Internet connection is available. You can proceed with execution.{W}")
     time.sleep(0.25)
 else:
-    print(f"{ERROR} No internet connection !{W}")
+    print(f"{please} No internet connection !{W}")
     exit()
-
-def clear_screen():
+clear_screen()
+def Banner():
     operating_system = os.name
     try:
-        if operating_system == 'posix': 
-            os.system('clear')
-        elif operating_system == 'nt': 
-            os.system('cls')
+        if (operating_system == 'posix'):
+            print(f"""
+┏────────────────────────────────────────────────────────────────┓
+│ {R}  {Y}  {G}  {W}                                                      │ 
+┣────────────────────────────────────────────────────────────────┫
+│      {G}\ | /{W}                                                     │
+│     {B}-- {R}O{B} --{W}                                                    │
+│       {G}/|\       {B} ___  ____________________________{W}             │
+│      {G}/\|/\      {B}|   |/   _____/__    ___/______   \{W}            │
+│     {G}/  |  \     {B}|   |\_____  \  |    |   |     ___/{W}            │
+│    {G}/\/\|/\/\    {B}|   |/        \ |    |   |    |{W}                │
+│   {G}/    |    \   {B}|___|_______  / |____|   |____| {Y}Version : 6{W}    │
+│  -     -     -        {B}      \/{W}                                 │     
+│ {Back.RED}{W} [Internet Speed Test Ping] {S}                                   │
+┣────────────────────────────────────────────────────────────────┫
+│    {Y}INSTAGRAM {W}| {B}https://www.instagram.com/r94xs/{W}                │
+│    {Y}GiTHub    {W}| {B}https://www.github.com/MohmmadALbaqer/{W}          │  
+┗────────────────────────────────────────────────────────────────┛
+""")
+        elif (operating_system == 'nt'):
+            print(f"""
+┏────────────────────────────────────────────────────────────────┓
+│ {R}● {Y}● {G}● {W}                                                         │ 
+┣────────────────────────────────────────────────────────────────┫
+│      {G}\ | /{W}                                                     │
+│     {B}-- {R}O{B} --{W}                                                    │
+│       {G}/|\       {B} ___  ____________________________{W}             │
+│      {G}/\|/\      {B}|   |/   _____/__    ___/______   \{W}            │
+│     {G}/  |  \     {B}|   |\_____  \  |    |   |     ___/{W}            │
+│    {G}/\/\|/\/\    {B}|   |/        \ |    |   |    |{W}                │
+│   {G}/    |    \   {B}|___|_______  / |____|   |____| {Y}Version : 5{W}    │
+│  -     -     -        {B}      \/{W}                                 │     
+│ {Back.RED}{W} [Internet Speed Test Ping] {S}                                   │
+┣────────────────────────────────────────────────────────────────┫
+│    {Y}INSTAGRAM {W}| {B}https://www.instagram.com/r94xs/{W}                │
+│    {Y}GiTHub    {W}| {B}https://www.github.com/MohmmadALbaqer/{W}          │  
+┗────────────────────────────────────────────────────────────────┛
+""")
         else:
-            print(f"[!] System unknown!{Style.RESET_ALL}")
+            print(f"{please} System unknown!{S}")
     except Exception as e:
-        print(f"[ERROR]{W}: {e}")
-clear_screen()
+        print(f"{ERROR}{W}: {e}")
 
-print(rf"""
-      {G}\ | /
-     {B}-- {R}O{B} --{W}
-       {G}/|\        {B}___  ____________________________{W}
-      {G}/\|/\      {B}|   |/   _____/__    ___/______   \{W}
-     {G}/  |  \     {B}|   |\_____  \  |    |   |     ___/{W} 
-    {G}/\/\|/\/\    {B}|   |/        \ |    |   |    |{W}
-   {G}/    |    \  {B} |___|_______  / |____|   |____|{Y} Version : 5{W}
-  {B}-     -     -{W}              {B}\/{W}
-{Back.RED}{W} [Internet Speed Test Ping] {Style.RESET_ALL}
-{R}+------------------------------------------------------------------+
-{R}|{G} GitHub{W} : {B}MohmmadALbaqer {W}|{Y} https://www.github.com/MohmmadALbaqer/ {R}|
-{R}|{G} Instagram{W} :{B} r94xs {W}      |{Y} https://www.instagram.com/r94xs/       {R}|
-{R}+------------------------------------------------------------------+{W}""")
+Banner()
 
-print(f"{B}[{G}+{B}] {Fore.WHITE}If you are waiting for a long time in downloading click to exit {Y}[Ctrl {W}+{Y} C]{W}")
+def NetWorkWindows():
+    ssid = None
+    ip_address = None
+
+    ssid_result = subprocess.run(['netsh', 'wlan', 'show', 'interfaces'], capture_output=True, text=True)
+    ssid_info = ssid_result.stdout.split('\n')
+    for line in ssid_info:
+        if 'SSID' in line and 'BSSID' not in line:
+            ssid = line.split(':')[-1].strip()
+            break
+
+    ip_result = subprocess.run(['ipconfig'], capture_output=True, text=True)
+    ip_info = ip_result.stdout.split('\n')
+    for line in ip_info:
+        if 'IPv4 Address' in line or 'IPv4 Address. . . . . . . . . . . :' in line:
+            ip_address = line.split(':')[-1].strip()
+            if re.match(r'^\d+\.\d+\.\d+\.\d+$', ip_address): 
+                break
+
+    return {'IPAddress': ip_address, 'SSID': ssid}
+
+def NetWorkLinux():
+    result = subprocess.run(['ip', 'addr', 'show'], capture_output=True, text=True)
+    ip_info = result.stdout.split('\n')
+    ip_address = None
+    for line in ip_info:
+        if 'inet ' in line and not line.strip().startswith('inet 127.'):
+            ip_address = line.split()[1].split('/')[0]
+            break
+
+    return {'IPAddress': ip_address}
+
+def NetWork():
+    os_system = platform.system()
+    if os_system == "Windows":
+        print(f"{INFO} system is:{G} Windowd{W}")
+        return NetWorkWindows()
+    elif os_system == "Linux":
+        print(f"{INFO} system is:{G} Linux{W}")
+        return NetWorkLinux()
+    else:
+        return {'IPAddress': f'{Y}Not available now in you system!{W}'}
+
+if __name__ == "__main__":
+    network_info = NetWork()
+    print(f"{INFO} IPAddress:{G}", network_info['IPAddress'])
+    if 'SSID' in network_info:
+        print(f"{INFO} SSID:{G}", network_info['SSID'])
+
+print(f"{sign} {W}If you are waiting for a long time in downloading click to exit {Y}[Ctrl {W}+{Y} C]{W}")
 init(autoreset=True)
-print(f"{B}[{G}*{B}] {Y}Downloading to servers and information at internet speed.{W}")
-st = speedtest.Speedtest()
-st.get_best_server()
+print(f"{sign} {Y}Downloading to servers and information at internet speed.{W}")
 
-for _ in tqdm(range(10), colour="green", desc=f"{INFO} Finding  Optimal  Server"):
-    sleep(0.05)
+def show_loading():
+    spinner = [f'{R}-{W}', f'{G}\\{W}', f'{B}|{W}', f'{Y}/{W}']
+    index = 0
+    while not stop_loading:
+        print(f"{sign}{W} Please waiting {spinner[index]}", end='\r', flush=True)
+        index = (index + 1) % len(spinner)
+        time.sleep(0.2)
 
-st.download()
-for _ in tqdm(range(10), colour="yellow", desc=f"{INFO} Getting {W}[{Y}Download{W}] {M}Speed"):
-    sleep(0.05)
+def main_task():
+    global stop_loading
+    stop_loading = False
+    loading_thread = threading.Thread(target=show_loading)
+    loading_thread.start()
 
-st.upload()
-for _ in tqdm(range(10), colour="red", desc=f"{INFO} Getting  {W}[{Y}Upload{W}] {M} Speed"):
-    sleep(0.05)
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    stop_loading = True
+    loading_thread.join()
+    
+    sys.stdout.write("\r\033[K")
+    print(date_day)
+    for _ in tqdm(range(10), colour="green", desc=f"{INFO} Finding  Optimal  Server"):
+        sleep(0.05)
 
-res_dict = st.results.dict()
+    st.download()
+    for _ in tqdm(range(10), colour="yellow", desc=f"{INFO} Getting {W}[{Y}Download{W}] {M}Speed"):
+        sleep(0.05)
 
-dwnl = f"{res_dict['download'] / 10**6:.2f}"
-upl = f"{res_dict['upload'] / 10**6:.2f}"
+    st.upload()
+    for _ in tqdm(range(10), colour="red", desc=f"{INFO} Getting  {W}[{Y}Upload{W}] {M} Speed"):
+        sleep(0.05)
 
-table = PrettyTable()
+    res_dict = st.results.dict()
 
-table.field_names = [f"{M}ID{W}", f"{B}INFORMATION{W}", f"{R}Information results{W}"]
+    dwnl = f"{res_dict['download'] / 10**6:.2f}"
+    upl = f"{res_dict['upload'] / 10**6:.2f}"
+    print("                                  ")
+    table = PrettyTable()
 
-table.add_row([f"{G}1{W}", f"{Y}Download{W}", f"{B}{dwnl} {M}Mbps{W} ({B}{float(dwnl) * 0.125:.2f} {G}MB/s{W})"])
-table.add_row([f"{G}2{W}", f"{Y}Upload{W}", f"{B}{upl} {M}Mbps{W} ({B}{float(upl) * 0.125:.2f} {G}MB/s{W})"])
-table.add_row([f"{G}3{W}", f"{Y}Ping{W}", f"{B}{res_dict['ping']:.2f} {G}ms{W}"])
-table.add_row([f"{G}4{W}", f"{Y}HOST{W}", res_dict['server']['host']])
-table.add_row([f"{G}5{W}", f"{Y}SPONSOR{W}", res_dict['server']['sponsor']])
-table.add_row([f"{G}6{W}", f"{Y}ISP{W}", res_dict['client']['isp']])
-table.add_row([f"{G}7{W}", f"{Y}Country{W}", res_dict['client']['country']])
-table.add_row([f"{G}8{W}", f"{Y}URL{W}", st.results.share()])
-table.add_row([f"{G}9{W}", f"{Y}Hosted By{W}", res_dict['server']['host']])
-packet_loss = res_dict.get('packetLoss', 'N/A')
-table.add_row([f"{G}10{W}", f"{Y}Packet Loss{W}", f"{B}{packet_loss}%{W}"])
-table.add_row([f"{G}11{W}", f"{Y}Server ID{W}", res_dict['server']['id']])
-table.add_row([f"{G}12{W}", f"{Y}ISP Rating{W}", res_dict['client']['isprating']])
+    table.field_names = [f"{M}ID{W}", f"{B}INFORMATION{W}", f"{R}Information results{W}"]
 
-for field in table.field_names:
-    table.align[field] = "l"
+    table.add_row([f"{G}1{W}", f"{Y}Download{W}", f"{B}{dwnl} {M}Mbps{W} ({B}{float(dwnl) * 0.125:.2f} {G}MB/s{W})"])
+    table.add_row([f"{G}2{W}", f"{Y}Upload{W}", f"{B}{upl} {M}Mbps{W} ({B}{float(upl) * 0.125:.2f} {G}MB/s{W})"])
+    table.add_row([f"{G}3{W}", f"{Y}Ping{W}", f"{B}{res_dict['ping']:.2f} {G}ms{W}"])
+    table.add_row([f"{G}4{W}", f"{Y}HOST{W}", res_dict['server']['host']])
+    table.add_row([f"{G}5{W}", f"{Y}SPONSOR{W}", res_dict['server']['sponsor']])
+    table.add_row([f"{G}6{W}", f"{Y}ISP{W}", res_dict['client']['isp']])
+    table.add_row([f"{G}7{W}", f"{Y}Country{W}", res_dict['client']['country']])
+    table.add_row([f"{G}8{W}", f"{Y}URL{W}", st.results.share()])
+    table.add_row([f"{G}9{W}", f"{Y}Hosted By{W}", res_dict['server']['host']])
+    packet_loss = res_dict.get('packetLoss', 'N/A')
+    table.add_row([f"{G}10{W}", f"{Y}Packet Loss{W}", f"{B}{packet_loss}%{W}"])
+    table.add_row([f"{G}11{W}", f"{Y}Server ID{W}", res_dict['server']['id']])
+    table.add_row([f"{G}12{W}", f"{Y}ISP Rating{W}", res_dict['client']['isprating']])
 
-print(table)
+    for field in table.field_names:
+        table.align[field] = "l"
+    print(table)
 
-now = datetime.datetime.now()
-formatted_time = now.strftime("%I:%M %p")
-formatted_day = now.strftime("%A")
-print(f"{B}[{G}Today{B}] {W}({Y}{formatted_day}{W} {M}{now:%B %D %Y}{W}) {B}[{G}Time{B}] {Y}[{R}{formatted_time}{Y}]{W}")
+    now = datetime.datetime.now()
+    formatted_time = now.strftime("%I:%M %p")
+    formatted_day = now.strftime("%A")
+    print(f"{B}[{G}Today{B}] {W}({Y}{formatted_day}{W} {M}{now:%B %D %Y}{W}) {B}[{G}Time{B}] {Y}[{R}{formatted_time}{Y}]{W}")
+
+main_task()
